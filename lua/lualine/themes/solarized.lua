d7cd5f59baa27541bb5f59d6b6408650868cc5ac
Richard Samuels <richard.l.samuels@gmail.com>
2024-02-11 20:03:59 -0500

    add lualine config

--- lua/lualine/themes/solarized.lua
+++ lua/lualine/themes/solarized.lua
@@ -1,37 +1,38 @@
-local colors = require('solarized.colors')
-
-local solarized = {}
-
-solarized.normal = {
-	a = {fg = colors.bg, bg = colors.accent, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-	c = {fg = colors.fg, bg = colors.selection},
+-- Copyright (c) 2020-2021 shadmansaleh
+-- MIT license, see LICENSE for more details.
+-- Credit: itchyny(lightline)
+-- stylua: ignore
+local colors = {
+  base03  =  '#002b36',
+  base02  =  '#073642',
+  base01  =  '#586e75',
+  base00  =  '#657b83',
+  base0   =  '#839496',
+  base1   =  '#93a1a1',
+  base2   =  '#eee8d5',
+  base3   =  '#fdf6e3',
+  yellow  =  '#b58900',
+  orange  =  '#cb4b16',
+  red     =  '#dc322f',
+  magenta =  '#d33682',
+  violet  =  '#6c71c4',
+  blue    =  '#268bd2',
+  cyan    =  '#2aa198',
+  green   =  '#859900',
 }
 
-solarized.insert = {
-	a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
+return {
+  normal = {
+    a = { fg = colors.base03, bg = colors.base1, gui = 'bold' },
+    b = { fg = colors.base03, bg = colors.base1 },
+    c = { fg = colors.base1, bg = colors.base02 },
+  },
+  insert = { a = { fg = colors.base03, bg = colors.green, gui = 'bold' } },
+  visual = { a = { fg = colors.base03, bg = colors.magenta, gui = 'bold' } },
+  replace = { a = { fg = colors.base03, bg = colors.red, gui = 'bold' } },
+  inactive = {
+    a = { fg = colors.base0, bg = colors.base02, gui = 'bold' },
+    b = { fg = colors.base03, bg = colors.base00 },
+    c = { fg = colors.base01, bg = colors.base02 },
+  },
 }
-
-solarized.visual = {
-	a = {fg = colors.bg, bg = colors.purple, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-}
-
-solarized.replace = {
-	a = {fg = colors.bg, bg = colors.red, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-}
-
-solarized.command = {
-	a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'},
-	b = {fg = colors.title, bg = colors.active},
-}
-
-solarized.inactive = {
-  a = {fg = colors.disabled, bg = colors.bg, gui = 'bold'},
-  b = {fg = colors.disabled, bg = colors.bg},
-  c = {fg = colors.disabled, bg = colors.selection}
-}
-
-return solarized
